49ca9086d87993f1b7c4b6c9d06ae3909ab24c4d
Vitor Cimetan <vitor_cimitan_9@hotmail.com>
2019-04-03 16:50:28 -0300

    Teste

---
+++ cliente.d
@@ -0,0 +1,9 @@
+module cliente;
+import std.stdio;
+
+void main()
+{
+    writeln("-----------------------------------------------");
+    writeln("Bem vindo ao \"QUEM SOU EU ? \"");
+    writeln("-----------------------------------------------");
+}
