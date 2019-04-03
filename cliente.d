4d25bbe96de9c2ff0c0e75b1bbff13834b002aa9
Allison Sampaio <allisonsampaiox@gmail.com>
2019-04-03 17:13:56 -0300

    Testando classes

--- cliente.d
+++ cliente.d
@@ -3,7 +3,22 @@ import std.stdio;
 
 void main()
 {
-    writeln("-----------------------------------------------");
-    writeln("Bem vindo ao \"QUEM SOU EU ? \"");
-    writeln("-----------------------------------------------");
+    writeln("------------------------------------------");
+    writeln("Bem vindo ao WhoAmI !");
+    writeln("------------------------------------------");
+}
+
+class Partida{
+	int id;
+	Jogador mestre;
+	string dica;
+	string resposta;
+	int tempo;
+	int pontuacao_maxima;
+}
+
+class Jogador{
+	int id;
+	string nome;
+	int pontuacao;
 }
